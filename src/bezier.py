e086e8fa9feee786f18bebf4f8341f9bf4828ead
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-03-16 13:16:15 +0700

    revise main

--- src/bezier.py
+++ src/bezier.py
@@ -37,37 +37,16 @@ def show_kurva_bezier_dnc(points, iterations, dnc):
     plt.show()
 
 
-import matplotlib.pyplot as plt
+def show_kurva_bezier_bf(points, iterations, t):
+    for j in range(iterations + 1):
+        intermediate_points = bf_kurva(points, j, t)
+        x_intermediate = [point[0] for point in intermediate_points]
+        y_intermediate = [point[1] for point in intermediate_points]
+        plt.plot(x_intermediate, y_intermediate, label=f"Iteration {j}")
 
-def show_kurva_bezier_bf(titik_kurva, titik_tengah, titik_awal):
-    if not isinstance(titik_kurva, list):
-        titik_kurva = [titik_kurva]
-    if not isinstance(titik_tengah, list):
-        titik_tengah = [titik_tengah]
-    if not isinstance(titik_awal, list):
-        titik_awal = [titik_awal]
-
-    x_kurva = [titik[0] for titik in titik_kurva]
-    y_kurva = [titik[1] for titik in titik_kurva]
-
-    if len(titik_tengah) > 0:
-        x_tengah = [titik[0] for titik in titik_tengah]
-        y_tengah = [titik[1] for titik in titik_tengah]
-    else:
-        x_tengah, y_tengah = [], []
-
-    if len(titik_awal) > 0:
-        x_awal = [titik[0] for titik in titik_awal]
-        y_awal = [titik[1] for titik in titik_awal]
-    else:
-        x_awal, y_awal = [], []
-
-    plt.plot(x_kurva, y_kurva, label="Kurva", color='blue')
-    plt.scatter(x_tengah, y_tengah, color='pink', label="Titik Tengah")
-    plt.scatter(x_awal, y_awal, color='red', label="Titik Awal")
     plt.xlabel('X')
     plt.ylabel('Y')
-    plt.title('Kurva Bézier')
+    plt.title('Intermediate Curves of Bézier Curve')
     plt.legend()
     plt.grid(True)
     plt.axis('equal')
