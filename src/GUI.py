2b96c975a33732b422cac8af5b40e9f13b8f283d
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-03-16 18:54:41 +0700

    initialize gui

--- src/GUI.py
+++ src/GUI.py
@@ -1,24 +1,61 @@
 import tkinter as tk
-from tkinter import *
+from tkinter import messagebox
+from bezier import *
+
+def create_input(frame, label, row, column):
+    label = tk.Label(frame, text=label)
+    label.grid(row=row, column=column, padx=5, pady=5)
+    entry = tk.Entry(frame)
+    entry.grid(row=row, column=column+1)
+    return entry
+
+def generate_bezier_curve():
+    try:
+        n = int(entry_points.get())
+        points = []
+        for i in range(n):
+            points.append(tuple(map(float, input_points[i].get().split())))
+        iterations = int(entry_iterations.get())
+
+        show_kurva_bezier(points, iterations, t=0.5, dnc=True)
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
 
 root = tk.Tk()
-root.geometry("500x500")
-root.title("Kurva Bézier")
+root.title("Bézier Curve Generator")
 
-label = tk.Label(root, text="Welcome to Bézier Curve Maker!", font=('Arial', 18))
-label.pack(padx=10, pady=10)
+label_title = tk.Label(root, text="Bézier Curve Generator", font=("Arial", 20), pady=20, padx=20) 
+label_title.pack()
 
-jumlah_titik = tk.Label(root, text="Input jumlah titik: ", height=3, font=('Arial', 12))
-jumlah_titik.pack()
+frame = tk.Frame(root, padx=20, pady=20)
+frame.pack()
 
-entry = tk.Entry(root)
-entry.pack()
+label_points = tk.Label(frame, text="Number of points (minimum 2):")
+label_points.grid(row=0, column=0)
+entry_points = tk.Entry(frame)
+entry_points.grid(row=0, column=1)
 
-textbox = tk.Text(root, height=3, font=('Arial', 12))
-textbox.pack(padx=10, pady=10)
+input_points = []
 
+def create_entry_points():
+    num_points = int(entry_points.get())
+    for widget in input_points:
+        widget.destroy()
+    input_points.clear()
+    for i in range(num_points):
+        if i == 0:
+            entry = create_input(frame, "Start Point:", i+1, 0)
+        elif i == num_points - 1:
+            entry = create_input(frame, "Final Point:", i+1, 0)
+        else:
+            entry = create_input(frame, f"Control Point {i}:", i+1, 0)
+        input_points.append(entry)
 
-btnshow = tk.Button(root, text="Show Bézier Curve", font=('Arial', 12))
-btnshow.pack(padx=10, pady=10)
+entry_points.bind("<FocusOut>", lambda event: create_entry_points())
+
+entry_iterations = create_input(frame, "Number of Iteration:", 7, 0)
+
+show_button = tk.Button(frame, text="Show the Bézier Curve", command=generate_bezier_curve)
+show_button.grid(row=9, column=0, columnspan=2, pady=10)
 
 root.mainloop()
